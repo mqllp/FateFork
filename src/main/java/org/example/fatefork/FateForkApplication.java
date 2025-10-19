e975811e3257f15457c369a149b3787abadad038
li-George <231250190@smail.nju.edu.cn>
2025-10-19 17:40:39 +0800

    firstwork

---
+++ src/main/java/org/example/fatefork/FateForkApplication.java
@@ -0,0 +1,13 @@
+package org.example.fatefork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FateForkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FateForkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fatefork/FateForkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fatefork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FateForkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
